149c12a84b73282e735bb7235872168f031795bf
makarovVlad <makaroff94@list.ru>
2022-07-21 23:29:06 +0300

    Добавьте файлы проекта.

---
+++ Model/AdmissionSpareParts.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineerKA_1._0.Model
+{
+    class AdmissionSpareParts
+    {
+    }
+}
---
+++ Model/DataWorker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineerKA_1._0.Model
+{
+   public static class DataWorker
+    {
+        //методы для работы с данными (загрузка из txt в таблицу и тд)
+    }
+}
